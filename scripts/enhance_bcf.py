abd1e9ce3ea86d58cdda11353767f73e48aa56e3
Lara Fuhrmann <lfuhrmann@bs-mbpr452.d.ethz.ch>
2021-02-16 16:48:46 +0100

    changed ambiguous_base_coverage_threshold to a frequency threshold, that way only variants with support of freq_thres percent of the reads are included in amgiguous computation

--- scripts/enhance_bcf.py
+++ scripts/enhance_bcf.py
@@ -6,19 +6,23 @@ from cyvcf2 import VCF, Writer
 
 
 def main(fname_in, fname_out, amibugous_base_coverage_threshold):
+    #amibugous_base_coverage_threshold: the frequency threshold to include a
+    #variant to the computation of ambiguous code. 
     vcf_reader = VCF(fname_in)
     vcf_writer = Writer(fname_out, vcf_reader)
 
     for variant in vcf_reader:
         base_list = [variant.REF] + variant.ALT
         coverage_list = variant.INFO.get('AD')
+        total_coverage = variant.INFO.get('DP')
+
         assert len(base_list) == len(coverage_list)
 
         # genotype 0 is reference (base is not really needed)
         genotype = [
             i
             for i, (base, coverage) in enumerate(zip(base_list, coverage_list))
-            if coverage >= amibugous_base_coverage_threshold
+            if coverage >= amibugous_base_coverage_threshold*total_coverage
         ]
 
         variant.genotypes = [[*genotype, False]]
@@ -30,4 +34,4 @@ def main(fname_in, fname_out, amibugous_base_coverage_threshold):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2], int(sys.argv[3]))
+    main(sys.argv[1], sys.argv[2], float(sys.argv[3]))
