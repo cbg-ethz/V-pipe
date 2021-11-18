3e955c6caa13896c576c65f894f342e009b12ab2
kpj <kim.philipp.jablonski@gmail.com>
2021-11-18 10:59:08 +0100

    Revert "[Mega-Linter] Apply linters fixes"
    This reverts commit e8ad82696545494625737087f897f35d9bad506d.

--- workflow/scripts/alignmentBias.py
+++ workflow/scripts/alignmentBias.py
@@ -11,70 +11,40 @@ __email__ = "v-pipe@bsse.ethz.ch"
 
 
 def parse_args():
-    """Set up the parsing of command-line arguments"""
+    """ Set up the parsing of command-line arguments """
 
     parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    requiredNamed = parser.add_argument_group("required named arguments")
+    requiredNamed = parser.add_argument_group('required named arguments')
     requiredNamed.add_argument(
-        "-r",
-        required=True,
-        default=None,
-        metavar="FASTA",
-        dest="reference_file",
-        type=str,
-        help="Referene sequence",
+        "-r", required=True, default=None, metavar='FASTA',
+        dest='reference_file', type=str, help="Referene sequence"
     )
     requiredNamed.add_argument(
-        "-b",
-        required=True,
-        default=None,
-        metavar="BAM",
-        dest="bamfile",
-        help="BAM file",
+        "-b", required=True, default=None, metavar='BAM', dest='bamfile',
+        help="BAM file"
     )
     requiredNamed.add_argument(
-        "-f",
-        required=True,
-        default=None,
-        metavar="FASTQ",
-        dest="fastq_file",
-        help="FASTQ file containing reads after QC",
+        "-f", required=True, default=None, metavar='FASTQ', dest='fastq_file',
+        help="FASTQ file containing reads after QC"
     )
     requiredNamed.add_argument(
-        "--hap",
-        required=True,
-        default=None,
-        metavar="FASTA",
-        dest="haplotype_seqs",
-        help="FASTA file containing haplotype sequences",
+        "--hap", required=True, default=None, metavar='FASTA',
+        dest='haplotype_seqs', help="FASTA file containing haplotype sequences"
     )
     requiredNamed.add_argument(
-        "-N",
-        required=False,
-        default="sample",
-        metavar="STR",
-        dest="sampleID",
-        help="Patient/sample identifiers",
+        "-N", required=False, default='sample', metavar='STR',
+        dest='sampleID', help="Patient/sample identifiers"
     )
     parser.add_argument(
-        "-p",
-        required=False,
-        default=False,
-        action="store_true",
-        dest="paired",
-        help="Indicate whether to simulate paired-end reads",
+        "-p", required=False, default=False, action='store_true',
+        dest='paired', help="Indicate whether to simulate paired-end reads"
     )
     parser.add_argument(
-        "-o",
-        required=False,
-        default="alignment_bias.tsv",
-        metavar="FILENAME",
-        dest="output_file",
-        type=str,
-        help="Output file",
+        "-o", required=False, default='alignment_bias.tsv', metavar='FILENAME',
+        dest='output_file', type=str,
+        help="Output file"
     )
 
     return parser.parse_args()
@@ -82,9 +52,8 @@ def parse_args():
 
 def hamming_dist(s1, s2):
     """Return the Hamming distance between sequences"""
-    assert len(s1) == len(s2), (
-        "Hamming distance is undefined for sequences " "differing in their lengths"
-    )
+    assert len(s1) == len(s2), ("Hamming distance is undefined for sequences "
+                                "differing in their lengths")
     return sum(el1 != el2 for el1, el2 in zip(s1, s2))
 
 
@@ -97,10 +66,10 @@ def main():
 
     hap_cnt = {}
     hap_coverage = {}
-    with pysam.AlignmentFile(args.bamfile, "rb") as alnfile:
+    with pysam.AlignmentFile(args.bamfile, 'rb') as alnfile:
         for read in alnfile.fetch(reference=reference.id, start=start, end=end):
             query_name = read.query_name
-            hapID = query_name.split("-")[0]
+            hapID = query_name.split('-')[0]
             if hapID in hap_cnt:
                 hap_cnt[hapID] += 1
             else:
@@ -108,7 +77,7 @@ def main():
 
             # Get the percent of aligned bases per read
             cigar_arr = read.get_cigar_stats()[0]
-            aux = cigar_arr[0] + cigar_arr[1]
+            aux = (cigar_arr[0] + cigar_arr[1])
             assert aux == read.query_alignment_length
             percent_aligned = read.query_alignment_length / read.infer_read_length()
             assert percent_aligned <= 1, f"{cigar_arr}"
@@ -127,7 +96,7 @@ def main():
     total = 0
     for read in SeqIO.parse(args.fastq_file, "fastq"):
         total += 1
-        hapID = read.id.split("-")[0]
+        hapID = read.id.split('-')[0]
         if hapID in hap_expected:
             hap_expected[hapID] += 1
         else:
@@ -147,31 +116,20 @@ def main():
     # Compute divergence from the reference sequence
     divergence = {}
     affine_pen = pairwise2.affine_penalty(-1, -0.1, True)
-    with open(args.haplotype_seqs, "r") as infile:
-        for record in SeqIO.parse(infile, "fasta"):
-            alignment = pairwise2.align.globalmc(
-                reference.seq,
-                record.seq,
-                1,
-                0,
-                affine_pen,
-                affine_pen,
-                one_alignment_only=True,
-            )
-            divergence[record.id] = (
-                hamming_dist(alignment[0][0], alignment[0][1]) / alignment[0][4]
-            )
+    with open(args.haplotype_seqs, 'r') as infile:
+        for record in SeqIO.parse(infile, 'fasta'):
+            alignment = pairwise2.align.globalmc(reference.seq, record.seq, 1, 0,
+                                                 affine_pen, affine_pen,
+                                                 one_alignment_only=True)
+            divergence[record.id] = hamming_dist(alignment[0][0],
+                                                 alignment[0][1]) / alignment[0][4]
 
-    with open(args.output_file, "w") as outfile:
-        outfile.write(
-            "SampleID\tHaplotypeID\tDivergence\tPercent-aligned\t"
-            "Percent-bases-aligned\n"
-        )
+    with open(args.output_file, 'w') as outfile:
+        outfile.write("SampleID\tHaplotypeID\tDivergence\tPercent-aligned\t"
+                      "Percent-bases-aligned\n")
         for k, v in percent_aligned.items():
-            outfile.write(
-                f"{args.sampleID}\t{k}\t{divergence[k]}\t{v}\t" f"{hap_coverage[k]}\n"
-            )
+            outfile.write(f"{args.sampleID}\t{k}\t{divergence[k]}\t{v}\t"
+                          f"{hap_coverage[k]}\n")
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- workflow/scripts/assemble_visualization_webpage.py
+++ workflow/scripts/assemble_visualization_webpage.py
@@ -28,25 +28,22 @@ def convert_vcf(fname):
         vcf_reader = vcf.Reader(fd)
 
         # check caller
-        caller_source = vcf_reader.metadata["source"][0].lower()
-        if caller_source.startswith("lofreq"):
-            mode = "lofreq"
-        elif caller_source.startswith("shorah"):
-            mode = "shorah"
+        caller_source = vcf_reader.metadata['source'][0].lower()
+        if caller_source.startswith('lofreq'):
+            mode = 'lofreq'
+        elif caller_source.startswith('shorah'):
+            mode = 'shorah'
         else:
-            raise RuntimeError(f"Invalid variant caller: {caller_source}")
+            raise RuntimeError(f'Invalid variant caller: {caller_source}')
 
         # parse records
         for record in vcf_reader:
-            if mode == "lofreq":
-                freq = round(record.INFO["AF"], 3)
-            elif mode == "shorah":
-                freq = round(
-                    np.mean(
-                        [v for k, v in record.INFO.items() if k.startswith("Freq")]
-                    ),
-                    3,
-                )
+            if mode == 'lofreq':
+                freq = round(record.INFO['AF'], 3)
+            elif mode == 'shorah':
+                freq = round(np.mean(
+                    [v for k, v in record.INFO.items() if k.startswith("Freq")]
+                ), 3)
 
             output.append(
                 {
@@ -54,7 +51,7 @@ def convert_vcf(fname):
                     "reference": record.REF,
                     "variant": [v.sequence for v in record.ALT],
                     "frequency": freq,
-                    "posterior": round(1 - 10 ** (-record.QUAL / 10), 3),
+                    "posterior": round(1 - 10**(-record.QUAL / 10), 3)
                 }
             )
 
@@ -68,9 +65,8 @@ def get_metainfo(metainfo_yaml):
         print(f'Parsing metainformation: "{metainfo_yaml}"')
         with open(metainfo_yaml) as fd:
             metainfo = yaml.load(fd.read(), Loader=yaml.SafeLoader)
-        assert (
-            type(metainfo) is dict
-        ), f"Probable syntax error in {metainfo_yaml} - need a dictionnary at top level, got {type(metainfo)} instead."
+        assert type(
+            metainfo) is dict, f'Probable syntax error in {metainfo_yaml} - need a dictionnary at top level, got {type(metainfo)} instead.'
         return metainfo
     else:
         print("No metainformation YAML provided, skipping.")
@@ -89,7 +85,7 @@ def parse_gff(fname):
                     {
                         "id": record.id,
                         "type": feature.type,
-                        "name": feature.qualifiers.get("Name", [feature.id])[0],
+                        "name": feature.qualifiers.get('Name', [feature.id])[0],
                         "start": int(feature.location.start),
                         "end": int(feature.location.end),
                     }
@@ -125,9 +121,8 @@ def get_gff_data(gff_dir, gff_metainfo={}):
     """Returns a map with filename key and gff json data."""
     if gff_metainfo == None:
         gff_metainfo = {}
-    assert (
-        type(gff_metainfo) is dict
-    ), f"Probable syntax error in metainfo YAML - need a dictionnary at [gff], got {type(gff_metainfo)} instead."
+    assert type(
+        gff_metainfo) is dict, f'Probable syntax error in metainfo YAML - need a dictionnary at [gff], got {type(gff_metainfo)} instead.'
 
     gff_map = {}
     if not gff_dir:
@@ -146,9 +141,8 @@ def get_primers_data(full_path, consensus, primers_metainfo={}):
     """Returns a map with filename key and primers json data."""
     if primers_metainfo == None:
         primers_metainfo = {}
-    assert (
-        type(primers_metainfo) is dict
-    ), f"Probable syntax error in metainfo YAML - need a dictionnary at [primers], got {type(primers_metainfo)} instead."
+    assert type(
+        primers_metainfo) is dict, f'Probable syntax error in metainfo YAML - need a dictionnary at [primers], got {type(primers_metainfo)} instead.'
 
     primers_map = {}
     if not full_path:
@@ -160,31 +154,26 @@ def get_primers_data(full_path, consensus, primers_metainfo={}):
     description = os.path.splitext(os.path.basename(full_path))[0]
     if description in primers_metainfo:
         description = primers_metainfo[description]
-    csv = pd.read_csv(full_path, sep=",")
-    primers = [
-        {"name": row[0], "seq": row[1].upper()} for row in csv[["name", "seq"]].values
-    ]
+    csv = pd.read_csv(full_path, sep=',')
+    primers = [{'name': row[0], "seq":row[1].upper()}
+               for row in csv[['name', 'seq']].values]
     primer_locations = []
     for entry in primers:
-        lookup_sequence = entry["seq"]
+        lookup_sequence = entry['seq']
         # If the sequence corresponds to a `right` primer, then reverse and
         # complement.
-        if "RIGHT" in entry["name"].upper():
+        if "RIGHT" in entry['name'].upper():
             seq = Seq(lookup_sequence)
             lookup_sequence = str(seq.reverse_complement())
         offsets = [
-            m.start()
-            for m in re.finditer("(?=" + lookup_sequence + ")", consensus_upper)
-        ]
+            m.start() for m in re.finditer(
+                '(?=' + lookup_sequence + ')',
+                consensus_upper)]
         for offset in offsets:
-            primer_locations.append(
-                {
-                    "name": entry["name"],
-                    "seq": entry["seq"],
-                    "start": offset,
-                    "end": offset + len(entry["seq"]) - 1,
-                }
-            )
+            primer_locations.append({'name': entry['name'],
+                                     'seq': entry['seq'],
+                                     'start': offset,
+                                     'end': offset + len(entry['seq']) - 1})
     if primer_locations:
         primers_map[description] = arrange_gff_data(primer_locations)
     else:
@@ -195,21 +184,16 @@ def get_primers_data(full_path, consensus, primers_metainfo={}):
 def convert_coverage(fname, sample_name=None):
     """Convert the read coverage to bp coverage."""
     print(f'Parsing coverage: "{fname}"')
-    csv = pd.read_csv(
-        fname, sep="\t", compression="infer", index_col=["ref", "pos"], header=0
-    )
+    csv = pd.read_csv(fname, sep="\t",compression='infer',
+                      index_col=['ref','pos'], header=0)
     col = None
     if len(csv.columns) == 1:
         if sample_name is not None and sample_name != csv.columns[0]:
-            print(
-                f'Ooops: column name "{csv.columns[0]}" in file is different from requested sample name "{sample_name}"!'
-            )
-        col = csv[csv.columns[0]]
+            print(f'Ooops: column name "{csv.columns[0]}" in file is different from requested sample name "{sample_name}"!')
+        col=csv[csv.columns[0]]
     else:
-        assert (
-            sample_name is not None
-        ), "Sample name is required when using combined coverage TSV."
-        col = csv[sample_name]
+        assert sample_name is not None, 'Sample name is required when using combined coverage TSV.'
+        col=csv[sample_name]
     return col.values.tolist()
 
 
@@ -227,33 +211,30 @@ def assemble_visualization_webpage(
 ):
     # parse the sample name
     path_components = os.path.normpath(wildcards_dataset).split(os.path.sep)
-    sample_name = "/".join(path_components[-2:])
+    sample_name = '/'.join(path_components[-2:])
 
     # parse the consensus sequence
     print(f'Parsing consensus: "{consensus_file}"')
     consensus = next(SeqIO.parse(consensus_file, "fasta")).seq.upper()
 
     # parse coverage file
-    coverage = convert_coverage(coverage_file, sample_name.replace("/", "-"))
+    coverage = convert_coverage(
+        coverage_file,
+        sample_name.replace('/', '-'))
 
     # load biodata in json format
     vcf_json = convert_vcf(vcf_file)
     metainfo = get_metainfo(metainfo_yaml)
-    gff_map = get_gff_data(
-        gff_directory, gff_metainfo=metainfo["gff"] if "gff" in metainfo else {}
-    )
-    primers_map = get_primers_data(
-        primers_file,
-        str(consensus),
-        primers_metainfo=metainfo["primers"] if "primers" in metainfo else {},
-    )
+    gff_map = get_gff_data(gff_directory,
+                           gff_metainfo=metainfo['gff'] if 'gff' in metainfo else {})
+    primers_map = get_primers_data(primers_file, str(consensus),
+                                   primers_metainfo=metainfo['primers'] if 'primers' in metainfo else {})
 
     # parse the reference name
-    reference_name = (
-        re.search(r"(.+)\.fa.*", os.path.basename(reference_file))
-        .group(1)
-        .replace("_", "&nbsp;")
-    )
+    reference_name = re.search(
+        r"(.+)\.fa.*",
+        os.path.basename(reference_file)
+    ).group(1).replace('_', '&nbsp;')
 
     embed_code = f"""
         var sample_name = \"{sample_name}\"
@@ -279,159 +260,72 @@ def assemble_visualization_webpage(
 def main():
     """Parse command line, run default functions."""
     # parse command line
-    parser = argparse.ArgumentParser(
-        description="Generate HTML visual report from VCF variants",
-        epilog="at minimum, either provide `-v` and `-c` or provide `-w`",
-    )
-    parser.add_argument(
-        "-f",
-        "--consensus",
-        metavar="FASTA",
-        required=False,
-        type=str,
-        dest="consensus_file",
-        help="consensus sequence of this sample",
-    )
-    parser.add_argument(
-        "-c",
-        "--coverage",
-        metavar="TSV",
-        required=False,
-        default="variants/coverage.tsv",
-        type=str,
-        dest="coverage_file",
-        help="global coverage table",
-    )
-    parser.add_argument(
-        "-v",
-        "--vcf",
-        metavar="VCF",
-        required=False,
-        type=str,
-        dest="vcf_file",
-        help="VCF containing the SNPs to be visualised",
-    )
-    parser.add_argument(
-        "-g",
-        "--gff",
-        metavar="DIR",
-        required=False,
-        type=str,
-        dest="gff_directory",
-        help="directory containing GFF annotations",
-    )
-    parser.add_argument(
-        "-p",
-        "--primers",
-        metavar="CSV",
-        required=False,
-        type=str,
-        dest="primers_file",
-        help="table with primers",
-    )
-    parser.add_argument(
-        "-m",
-        "--metainfo",
-        metavar="YAML",
-        required=False,
-        type=str,
-        dest="metainfo_yaml",
-        help="metainformation for the GFF and primers",
-    )
-    parser.add_argument(
-        "-t",
-        "--template",
-        metavar="HTML",
-        required=False,
-        default=f"{os.path.dirname(__file__)}/visualization.html",
-        type=str,
-        dest="html_file_in",
-        help="HTML template used to generate visual report",
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        metavar="HTML",
-        required=False,
-        type=str,
-        dest="html_file_out",
-        help="produced HTML report",
-    )
-    parser.add_argument(
-        "-w",
-        "--wildcards",
-        metavar="SAMPLE/DATE",
-        required=False,
-        type=str,
-        dest="wildcards_dataset",
-        help="sample's two-level directory hierarchy prefix",
-    )
-    parser.add_argument(
-        "-r",
-        "--reference",
-        metavar="FASTA",
-        required=False,
-        default="variants/cohort_consensus.fasta",
-        type=str,
-        dest="reference_file",
-        help="reference against which SNVs were called (e.g.: cohort's consensus)",
-    )
+    parser = argparse.ArgumentParser(description="Generate HTML visual report from VCF variants",
+                                     epilog="at minimum, either provide `-v` and `-c` or provide `-w`")
+    parser.add_argument('-f', '--consensus', metavar='FASTA', required=False,
+                        type=str, dest='consensus_file', help="consensus sequence of this sample")
+    parser.add_argument('-c', '--coverage', metavar='TSV', required=False,
+                        default='variants/coverage.tsv',
+                        type=str, dest='coverage_file', help="global coverage table")
+    parser.add_argument('-v', '--vcf', metavar='VCF', required=False,
+                        type=str, dest='vcf_file', help="VCF containing the SNPs to be visualised")
+    parser.add_argument('-g', '--gff', metavar='DIR', required=False,
+                        type=str, dest='gff_directory', help="directory containing GFF annotations")
+    parser.add_argument('-p', '--primers', metavar='CSV', required=False,
+                        type=str, dest='primers_file', help="table with primers")
+    parser.add_argument('-m', '--metainfo', metavar='YAML', required=False,
+                        type=str, dest='metainfo_yaml', help="metainformation for the GFF and primers")
+    parser.add_argument('-t', '--template', metavar='HTML', required=False,
+                        default=f'{os.path.dirname(__file__)}/visualization.html',
+                        type=str, dest='html_file_in', help="HTML template used to generate visual report")
+    parser.add_argument('-o', '--output', metavar='HTML', required=False,
+                        type=str, dest='html_file_out', help="produced HTML report")
+    parser.add_argument('-w', '--wildcards', metavar='SAMPLE/DATE', required=False,
+                        type=str, dest='wildcards_dataset', help="sample's two-level directory hierarchy prefix")
+    parser.add_argument('-r', '--reference', metavar='FASTA', required=False,
+                        default='variants/cohort_consensus.fasta',
+                        type=str, dest='reference_file', help="reference against which SNVs were called (e.g.: cohort's consensus)")
 
     args = parser.parse_args()
 
     # defaults which can be guess from one another
-    if (
-        args.vcf_file == None
-    ):  # e.g.: samples/140074_395_D02/20200615_J6NRK/variants/SNVs/snvs.vcf
-        assert (
-            args.wildcards_dataset != None
-        ), "cannot automatically find VCF without wildcards"
+    if args.vcf_file == None:  # e.g.: samples/140074_395_D02/20200615_J6NRK/variants/SNVs/snvs.vcf
+        assert args.wildcards_dataset != None, 'cannot automatically find VCF without wildcards'
         args.vcf_file = os.path.join(
-            args.wildcards_dataset, "variants", "SNVs", "snvs.vcf"
-        )
+            args.wildcards_dataset, 'variants', 'SNVs', 'snvs.vcf')
 
-    if (
-        args.consensus_file == None
-    ):  # e.g.: samples/140074_395_D02/20200615_J6NRK/references/ref_majority.fasta
-        assert (
-            args.wildcards_dataset != None
-        ), "cannot automatically find consensus without wildcards"
+    if args.consensus_file == None:  # e.g.: samples/140074_395_D02/20200615_J6NRK/references/ref_majority.fasta
+        assert args.wildcards_dataset != None, 'cannot automatically find consensus without wildcards'
         args.consensus_file = os.path.join(
-            args.wildcards_dataset, "references", "ref_majority.fasta"
-        )
+            args.wildcards_dataset, 'references', 'ref_majority.fasta')
 
     if args.wildcards_dataset == None:
-        assert (
-            args.vcf_file != None and args.consensus != None
-        ), "cannot deduce wilcards without a consensus and a vcf"
-        try1 = "/".join(os.path.normpath(args.vcf_file).split(os.path.sep)[-5:-3])
-        try2 = "/".join(os.path.normpath(args.consensus_file).split(os.path.sep)[-4:-2])
-        assert (
-            try1 == try2
-        ), f"cannot deduce wildcards automatically from <{args.vcf_file}> and <{args.consensus_file}>, please specify explicitly using `--wirdcards`"
+        assert args.vcf_file != None and args.consensus != None, 'cannot deduce wilcards without a consensus and a vcf'
+        try1 = '/'.join(os.path.normpath(args.vcf_file)
+                        .split(os.path.sep)[-5:-3])
+        try2 = '/'.join(os.path.normpath(args.consensus_file)
+                        .split(os.path.sep)[-4:-2])
+        assert try1 == try2, f'cannot deduce wildcards automatically from <{args.vcf_file}> and <{args.consensus_file}>, please specify explicitly using `--wirdcards`'
         args.wildcards_dataset = try1
 
     if args.html_file_out == None:
         args.html_file_out = os.path.join(
-            args.wildcards_dataset, "visualization", "index.html"
-        )
+            args.wildcards_dataset, 'visualization', 'index.html')
 
     # check mandatory files exist
-    for n, f in {
-        "vcf": args.vcf_file,
-        "consensus": args.consensus_file,
-        "coverage": args.coverage_file,
-        "template": args.html_file_in,
-    }.items():
+    for n, f in {'vcf': args.vcf_file,
+                 'consensus': args.consensus_file,
+                 'coverage': args.coverage_file,
+                 'template': args.html_file_in,
+                 }.items():
         if not os.path.exists(f):
             parser.error(f"{n} file <{f}> does not exist!")
 
     # check optional files exist if specified
-    for n, f in {
-        "gff": args.gff_directory,
-        "primers": args.primers_file,
-        "metainfo": args.metainfo_yaml,
-    }.items():
+    for n, f in {'gff': args.gff_directory,
+                 'primers': args.primers_file,
+                 'metainfo': args.metainfo_yaml,
+                 }.items():
         if f and not os.path.exists(f):
             parser.error(f"{n} file <{f}> does not exist!")
 
