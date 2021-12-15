22adf5557ccee7d4d988e6024f0f9e3d0ed85365
kpj <kim.philipp.jablonski@gmail.com>
2021-12-15 11:22:52 +0100

    Format shorah_default.py

--- resources/auxiliary_workflows/benchmark/resources/method_definitions/shorah_default.py
+++ resources/auxiliary_workflows/benchmark/resources/method_definitions/shorah_default.py
@@ -7,11 +7,18 @@ from pathlib import Path
 def main(fname_bam, fname_reference, fname_results, dname_work):
     dname_work.mkdir(parents=True, exist_ok=True)
     subprocess.run(
-        ["shorah", "shotgun", "-b", fname_bam.resolve(), "-f", fname_reference.resolve()],
-        cwd=dname_work
+        [
+            "shorah",
+            "shotgun",
+            "-b",
+            fname_bam.resolve(),
+            "-f",
+            fname_reference.resolve(),
+        ],
+        cwd=dname_work,
     )
 
-    (dname_work / 'snv' / 'SNVs_0.010000_final.vcf').rename(fname_results)
+    (dname_work / "snv" / "SNVs_0.010000_final.vcf").rename(fname_results)
 
 
 if __name__ == "__main__":
