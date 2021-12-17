bf7f056b62e65066882d3c42e18ee45cdff486a4
Lara Fuhrmann <lfuhrmann@bs-mbpr452.d.ethz.ch>
2021-12-17 17:45:47 +0100

    reformatted with black

--- workflow/scripts/aggregate_diversity.py
+++ workflow/scripts/aggregate_diversity.py
@@ -4,12 +4,23 @@ Script aggregating diversity measures from all samples into one file.
 """
 import pandas as pd
 
+
 def main(in_fnames_diversity, in_fnames_shannon, out_diversity_csv, out_shannon_csv):
-    merged_div_csv=pd.concat([pd.read_csv(path_div) for path_div in in_fnames_diversity])
+    merged_div_csv = pd.concat(
+        [pd.read_csv(path_div) for path_div in in_fnames_diversity]
+    )
     merged_div_csv.to_csv(out_diversity_csv)
 
-    merged_shan_csv=pd.concat([pd.read_csv(path_shan) for path_shan in in_fnames_shannon])
+    merged_shan_csv = pd.concat(
+        [pd.read_csv(path_shan) for path_shan in in_fnames_shannon]
+    )
     merged_shan_csv.to_csv(out_shannon_csv)
 
-if __name__ == '__main__':
-    main(snakemake.input.fnames_diversity, snakemake.input.fnames_shannon, snakemake.output.diversity_csv, snakemake.output.shannon_csv)
+
+if __name__ == "__main__":
+    main(
+        snakemake.input.fnames_diversity,
+        snakemake.input.fnames_shannon,
+        snakemake.output.diversity_csv,
+        snakemake.output.shannon_csv,
+    )
