b5ba386d98d1aeefafb0873f5c8b4b6adf505ab0
Lara Fuhrmann <lfuhrmann@bs-mbpr452.d.ethz.ch>
2022-02-06 17:48:23 +0100

    inclusion of new inference method for shorah

---
+++ resources/auxiliary_workflows/benchmark/resources/method_definitions/shorah_canary.py
@@ -0,0 +1,37 @@
+# PIP: git+https://github.com/spaceben/shorah@switch-to-poetry
+
+import subprocess
+from pathlib import Path
+
+def main(fname_bam, fname_reference, fname_results, dname_work):
+
+    # TODO: it seems like the region input does not have any effect on the
+    # windows that are created.
+    # Compared to original shorah we need the argument -r for this version.
+    region='MasterSequence:210-3500'
+
+    dname_work.mkdir(parents=True, exist_ok=True)
+    subprocess.run(
+        [
+            "shorah",
+            "shotgun",
+            "-b",
+            fname_bam.resolve(),
+            "-f",
+            fname_reference.resolve(),
+            "-r",
+            region,
+        ],
+        cwd=dname_work,
+    )
+
+    (dname_work / "snv" / "SNVs_0.010000_final.vcf").rename(fname_results)
+
+
+if __name__ == "__main__":
+    main(
+        Path(snakemake.input.fname_bam),
+        Path(snakemake.input.fname_reference),
+        Path(snakemake.output.fname_results),
+        Path(snakemake.output.dname_work),
+    )
