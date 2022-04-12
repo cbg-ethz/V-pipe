668babbc26fff7314dd698a0ec7cc4060f17b4f0
Lara Fuhrmann <lfuhrmann@bs-mbpr452.d.ethz.ch>
2022-04-12 11:30:06 +0200

    [Added] lofreq as method

---
+++ resources/auxiliary_workflows/benchmark/resources/method_definitions/lofreq.py
@@ -0,0 +1,28 @@
+# CONDA: lofreq = 2.1.5
+import subprocess
+from pathlib import Path
+
+
+def main(fname_bam, fname_reference, fname_results, dname_work):
+    dname_work.mkdir(parents=True, exist_ok=True)
+    subprocess.run(
+        [
+            "lofreq",
+            "call",
+            "-f",
+            fname_reference.resolve(),
+            "-o",
+            fname_results.resolve(),
+            fname_bam.resolve(),
+        ],
+        cwd=dname_work,
+    )
+
+
+if __name__ == "__main__":
+    main(
+        Path(snakemake.input.fname_bam),
+        Path(snakemake.input.fname_reference),
+        Path(snakemake.output.fname_results),
+        Path(snakemake.output.dname_work),
+    )
