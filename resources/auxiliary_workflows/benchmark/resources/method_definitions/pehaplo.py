edfc24a12571d6d61b20d6ed64000ec14d0c0316
kpj <kim.philipp.jablonski@gmail.com>
2022-04-21 14:40:07 +0200

    Add pehaplo method (draft due to crash)

---
+++ resources/auxiliary_workflows/benchmark/resources/method_definitions/pehaplo.py
@@ -0,0 +1,48 @@
+# GROUP: global
+# CONDA: pehaplo = v0.1_dev05
+# CONDA: samtools = 1.15.1
+
+import subprocess
+from pathlib import Path
+
+
+def main(fname_bam, fname_reference, fname_marker, dname_work):
+    dname_work.mkdir(parents=True, exist_ok=True)
+
+    subprocess.run(
+        [
+            "samtools",
+            "fastq",
+            "-1",
+            dname_work / "reads.R1.fastq",
+            "-2",
+            dname_work / "reads.R2.fastq",
+            fname_bam,
+        ]
+    )
+    subprocess.run(
+        [
+            "pehaplo.py",
+            "-f1",
+            (dname_work / "reads.R1.fastq").resolve(),
+            "-f2",
+            (dname_work / "reads.R2.fastq").resolve(),
+            "--overlap_len",
+            str(int(snakemake.wildcards.read_length) // 4),
+            "--read_len",
+            snakemake.wildcards.read_length,
+        ],
+        cwd=dname_work,
+    )
+
+    # TODO: select correct result
+    print(list(dname_work.iterdir()))
+
+
+if __name__ == "__main__":
+    main(
+        Path(snakemake.input.fname_bam),
+        Path(snakemake.input.fname_reference),
+        Path(snakemake.output.fname_marker),
+        Path(snakemake.output.dname_work),
+    )
