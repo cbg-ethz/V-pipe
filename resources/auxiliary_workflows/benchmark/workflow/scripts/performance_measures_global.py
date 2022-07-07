c3345d1311890d26e3231d44bde90ab844a86ae2
kpj <kim.philipp.jablonski@gmail.com>
2022-07-07 19:00:20 +0200

    Small improvement

--- resources/auxiliary_workflows/benchmark/workflow/scripts/performance_measures_global.py
+++ resources/auxiliary_workflows/benchmark/workflow/scripts/performance_measures_global.py
@@ -278,7 +278,7 @@ def compute_pr(df_pred, df_true, thres=0.05):
 
         # true positive: predicted seq appears in ground truth
         # false positive: predicted seq does not appear in ground truth
-        for row in tqdm(df_group.itertuples(), leave=False):
+        for row in tqdm(df_group.itertuples(), total=df_group.shape[0], leave=False):
             ser_dist = df_true["sequence"].apply(
                 lambda x: compute_dist(x, row.sequence)
             )
@@ -292,7 +292,7 @@ def compute_pr(df_pred, df_true, thres=0.05):
         # false negative: ground truth sequence was not predicted
         # single prediction should not map to multiple ground truth seqs
         df_cur = df_group.copy()
-        for row in tqdm(df_true.itertuples(), leave=False):
+        for row in tqdm(df_true.itertuples(), total=df_true.shape[0], leave=False):
             ser_dist = df_cur["sequence"].apply(lambda x: compute_dist(x, row.sequence))
             passed_thres = (ser_dist <= thres).any()
 
