483bd0d88e9d9369927fc33d1924541cb0be165b
Lara Fuhrmann <lfuhrmann@ethz.ch>
2022-07-18 15:40:19 +0200

    [local_haplo_setup] correction output file of shorah_default

--- resources/auxiliary_workflows/benchmark/resources/method_definitions/shorah_default.py
+++ resources/auxiliary_workflows/benchmark/resources/method_definitions/shorah_default.py
@@ -22,8 +22,9 @@ def main(fname_bam, fname_reference, fname_result, fname_result_haplos, dname_wo
             cwd=dname_work,
             check=True,
         )
+        (dname_work / "snv" / "SNVs_0.010000_final.vcf").rename(fname_result)
 
-        open(fname_result_haplos, 'a').close()
+    open(fname_result_haplos, 'a').close()
 
 if __name__ == "__main__":
     main(
