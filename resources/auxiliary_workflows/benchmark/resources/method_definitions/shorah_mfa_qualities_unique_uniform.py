dbc64130f6ac7c6ba2b6466161a20f5b18bde655
Lara Fuhrmann <lfuhrmann@ethz.ch>
2022-07-25 15:35:05 +0200

    [local_haplotype_setup] added amplicon illumina simulaation, adapted methods for that

--- resources/auxiliary_workflows/benchmark/resources/method_definitions/shorah_mfa_qualities_unique_uniform.py
+++ resources/auxiliary_workflows/benchmark/resources/method_definitions/shorah_mfa_qualities_unique_uniform.py
@@ -22,7 +22,7 @@ def gunzip(source_filepath, dest_filepath, block_size=65536):
                 d_file.write(block)
 
 
-def main(fname_bam, fname_reference, fname_results_snv, fname_result_haplos, dname_work):
+def main(fname_bam, fname_reference, fname_insert_bed, fname_results_snv, fname_result_haplos, dname_work):
 
     genome_size = str(fname_bam).split('genome_size~')[1].split('__coverage')[0]
     alpha = 0.00001
@@ -30,25 +30,52 @@ def main(fname_bam, fname_reference, fname_results_snv, fname_result_haplos, dna
     n_mfa_starts = 1
 
     dname_work.mkdir(parents=True, exist_ok=True)
-    subprocess.run(
-        [
-            "shorah",
-            "shotgun",
-            "-b",
-            fname_bam.resolve(),
-            "-f",
-            fname_reference.resolve(),
-            "--inference",
-            "mean_field_approximation",
-            "--alpha",
-            str(alpha),
-            "--n_max_haplotypes",
-            str(n_max_haplotypes),
-            "--n_mfa_starts",
-            str(n_mfa_starts),
-        ],
-        cwd=dname_work,
-    )
+
+    if fname_insert_bed == "":
+        # no insert file --> shotgun mode
+        subprocess.run(
+            [
+                "shorah",
+                "shotgun",
+                "-b",
+                fname_bam.resolve(),
+                "-f",
+                fname_reference.resolve(),
+                "--inference",
+                "mean_field_approximation",
+                "--alpha",
+                str(alpha),
+                "--n_max_haplotypes",
+                str(n_max_haplotypes),
+                "--n_mfa_starts",
+                str(n_mfa_starts),
+            ],
+            cwd=dname_work,
+        )
+    else:
+        # insert file --> amplicon mode
+        subprocess.run(
+            [
+                "shorah",
+                "shotgun",
+                "-b",
+                fname_bam.resolve(),
+                "-f",
+                fname_reference.resolve(),
+                "--inference",
+                "mean_field_approximation",
+                "--alpha",
+                str(alpha),
+                "--n_max_haplotypes",
+                str(n_max_haplotypes),
+                "--n_mfa_starts",
+                str(n_mfa_starts),
+                "--insert-file",
+                fname_insert_bed.resolve(),
+            ],
+            cwd=dname_work,
+        )
+
 
     (dname_work / "snv" / "SNVs_0.010000_final.vcf").rename(fname_results_snv)
     open(fname_result_haplos, 'a').close()
@@ -58,6 +85,7 @@ if __name__ == "__main__":
     main(
         Path(snakemake.input.fname_bam),
         Path(snakemake.input.fname_reference),
+        Path(snakemake.input.fname_insert_bed),
         Path(snakemake.output.fname_result),
         Path(snakemake.output.fname_result_haplos),
         Path(snakemake.output.dname_work),
