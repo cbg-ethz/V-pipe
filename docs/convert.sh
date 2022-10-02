57b386645e6ba7a3a9247ef26ca4dfcb96172ddc
Ivan Blagoev Topolsky <ivan.topolsky@sib.swiss>
2022-10-02 02:22:32 +0200

    Tutorial run their own branch of V-pipe
     - option to patch-in branch

--- docs/convert.sh
+++ docs/convert.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-echo "current branch:"
-git branch --show-current
+branch="$(git branch --show-current)"
+default="master"
+echo "current branch: ${branch}"
+
+if [[ "${1}" == "branch" ]]; then
+	if [[ "${branch}" != "${default}" ]]; then
+		echo "patching tutorials..."
+		sed -ri "s@(quick_install.sh) +(-[^b])@\1 -b ${branch} \2@g" ./*.md
+		## example command to keep the output with errors
+		# jupytext --to ipynb --pipe-fmt ipynb --pipe 'jupyter nbconvert --to ipynb --execute --allow-errors --stdin --stdout' ./*.md
+	else
+		echo -e "\e[32;1mNo need to patch branch.\e[0m The default branch is already \`${branch}\`"
+	fi
+elif [[ "${branch}" != "${default}" ]]; then
+	echo -e "\e[33;1mWarning: installer in tutorials will still clone the \`${default}\` branch!\e[0m Use \`$0 branch\` to patch them on the fly and instll the current branch \`${branch}\`"
+fi
 
 # create Jupyter Notebooks for all Markdown files
-jupytext --set-formats ipynb,md --execute ./*.md
+exec jupytext --set-formats ipynb,md --execute ./*.md
