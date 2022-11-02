72452b32feca4b5affe9663b41c9e701ca895069
Ivan Blagoev Topolsky <ivan.topolsky@bsse.ethz.ch>
2022-11-02 19:38:49 +0100

    Tutorial CI/CD tweaks
     - do not crash on tag that have been checked out in grafted mode
       (without a branch name)

--- docs/convert.sh
+++ docs/convert.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
+# branch (for usual PR and pushes)
 branch="$(git branch --show-current)"
+# fall back on tags if branch was left empty (checked out in grafted/detached head mode, e.g. when testing a tag)
+branch="${branch:-$(git describe --tags)}"
+# fall back to hash
+branch="${branch:-$(git rev-parse HEAD)}"
+
 default="master"
 echo "current branch: ${branch}"
 
 if [[ "${1}" == "branch" ]]; then
-	if [[ "${branch}" != "${default}" ]]; then
+	if [[ -z "${branch}" ]]; then
+		echo -e "\e[31;1mCannot determine current branch or tag!\e[0m The installer in tutorials will still clone the \`${default}\` branch."
+	elif [[ "${branch}" != "${default}" ]]; then
 		echo "patching tutorials..."
-		sed -ri "s@(quick_install.sh) +(-[^b])@\1 -b ${branch} \2@g;s@(V-pipe)/${default}(/utils)@\1/${branch}\2@g" ./*.md
+		sed -ri "s@(quick_install.sh) +(-[^b])@\1 -b ${branch} \2@g;s@(V-pipe)/${default}(/utils)@\1/${branch}\2@g" ./tutorial*.md
 		## example command to keep the output with errors
 		# jupytext --to ipynb --pipe-fmt ipynb --pipe 'jupyter nbconvert --to ipynb --execute --allow-errors --stdin --stdout' ./*.md
 	else
