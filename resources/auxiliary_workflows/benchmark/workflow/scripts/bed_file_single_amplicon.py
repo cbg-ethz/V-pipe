388c89da892dcf91843bdec8fbf2198c9a6134d0
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2022-12-16 12:00:00 +0100

    include single_amplicon mode

---
+++ resources/auxiliary_workflows/benchmark/workflow/scripts/bed_file_single_amplicon.py
@@ -0,0 +1,16 @@
+from Bio import SeqIO
+
+def main(fname_reference, fname_insert_bed, genome_size):
+
+    for seq in SeqIO.parse(fname_reference, "fasta"):
+        seq_id = seq.id
+
+    with open(fname_insert_bed, 'w') as f:
+        f.write(str(seq_id)+'  1       '+str(genome_size)+'     scheme_INSERT_1 1       +')
+
+if __name__ == "__main__":
+    main(
+        Path(snakemake.input.fname_reference),
+        Path(snakemake.output.fname_insert_bed),
+        snakemake.wildcards.genome_size,
+    )
