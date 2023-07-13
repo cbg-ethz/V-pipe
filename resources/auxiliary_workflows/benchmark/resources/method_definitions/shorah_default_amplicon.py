8fc931a7bb67022726e8b3742dff000ae8593849
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2023-07-13 14:04:21 +0200

    add old shorah amplicon to compare illumina

--- resources/auxiliary_workflows/benchmark/resources/method_definitions/shorah_default_amplicon.py
+++ resources/auxiliary_workflows/benchmark/resources/method_definitions/shorah_default_amplicon.py
@@ -9,42 +9,48 @@ from os.path import isfile, join
 from Bio import SeqIO
 
 
-def main(fname_bam, fname_reference, fname_result, fname_result_haplos, dname_work):
+def main(fname_bam, fname_reference, fname_result, fname_result_haplos, dname_work,seq_tech, genome_size,read_length):
     dname_work.mkdir(parents=True, exist_ok=True)
-    subprocess.run(
-        [
-            "shorah",
-            "amplicon",
-            "-b",
-            fname_bam.resolve(),
-            "-f",
-            fname_reference.resolve(),
-        ],
-        cwd=dname_work,
-        check=True,
-    )
 
-    (dname_work / "SNVs_0.010000_final.vcf").rename(fname_result)
+    if (read_length > genome_size) & seq_tech == 'illumina':
+        open(fname_result_haplos, 'a').close()
+        open(fname_result, 'a').close()
+    else:
 
-    mypath = (dname_work).resolve()
-    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    for file in onlyfiles:
-        if file.endswith('reads-support.fas'):
-            fname_haplos = (dname_work / "support" / onlyfiles[0]).resolve()
-            (dname_work  / file).rename(fname_result_haplos)
+        subprocess.run(
+            [
+                "shorah",
+                "amplicon",
+                "-b",
+                fname_bam.resolve(),
+                "-f",
+                fname_reference.resolve(),
+            ],
+            cwd=dname_work,
+            check=True,
+        )
 
-    # fix frequency information
+        (dname_work / "SNVs_0.010000_final.vcf").rename(fname_result)
 
-    freq_list = []
-    for record in SeqIO.parse(fname_result_haplos, "fasta"):
-        freq_list.append(float(record.description.split('ave_reads=')[-1]))
-    norm_freq_list = [float(i)/sum(freq_list) for i in freq_list]
+        mypath = (dname_work).resolve()
+        onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        for file in onlyfiles:
+            if file.endswith('reads-support.fas'):
+                fname_haplos = (dname_work / "support" / onlyfiles[0]).resolve()
+                (dname_work  / file).rename(fname_result_haplos)
 
-    record_list = []
-    for idx, record in enumerate(SeqIO.parse(fname_result_haplos, "fasta")):
-        record.description = f"freq:{norm_freq_list[idx]}"
-        record_list.append(record)
-    SeqIO.write(record_list, fname_result_haplos, "fasta")
+        # fix frequency information
+
+        freq_list = []
+        for record in SeqIO.parse(fname_result_haplos, "fasta"):
+            freq_list.append(float(record.description.split('ave_reads=')[-1]))
+        norm_freq_list = [float(i)/sum(freq_list) for i in freq_list]
+
+        record_list = []
+        for idx, record in enumerate(SeqIO.parse(fname_result_haplos, "fasta")):
+            record.description = f"freq:{norm_freq_list[idx]}"
+            record_list.append(record)
+        SeqIO.write(record_list, fname_result_haplos, "fasta")
 
 
 if __name__ == "__main__":
@@ -54,4 +60,7 @@ if __name__ == "__main__":
         Path(snakemake.output.fname_result),
         Path(snakemake.output.fname_result_haplos),
         Path(snakemake.output.dname_work),
+        snakemake.wildcards.seq_tech,
+        snakemake.wildcards.genome_size,
+        snakemake.wildcards.read_length,
     )
