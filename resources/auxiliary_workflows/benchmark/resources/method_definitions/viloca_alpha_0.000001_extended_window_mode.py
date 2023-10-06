9e2f24248ec2c0314d0c58d8f134b9045a26db89
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2023-10-06 17:04:33 +0200

    [indels] update method run file with threads and without haplo files

--- resources/auxiliary_workflows/benchmark/resources/method_definitions/viloca_alpha_0.000001_extended_window_mode.py
+++ resources/auxiliary_workflows/benchmark/resources/method_definitions/viloca_alpha_0.000001_extended_window_mode.py
@@ -20,7 +20,7 @@ def gunzip(source_filepath, dest_filepath, block_size=65536):
             else:
                 d_file.write(block)
 
-def main(fname_bam, fname_reference,fname_insert_bed, fname_results_snv, fname_result_haplos, dname_work, threads=1):
+def main(fname_bam, fname_reference,fname_insert_bed, fname_results_snv, fname_result_haplos, dname_work, threads):
 
     genome_size = str(fname_bam).split('genome_size~')[1].split('__coverage')[0]
     alpha = 0.000001
@@ -91,38 +91,7 @@ def main(fname_bam, fname_reference,fname_insert_bed, fname_results_snv, fname_r
         )
 
     (dname_work / "snv" / "SNVs_0.010000_final.vcf").rename(fname_results_snv)
-
-
-    mypath = (dname_work /'support').resolve()
-    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    print('onlyfiles', onlyfiles)
-    for file in onlyfiles:
-        if 'reads-support.fas' in file:
-            file_name = onlyfiles[0]
-            fname_haplos = (dname_work / "support" / onlyfiles[0]).resolve()
-            if file.endswith('.gz'):
-                fname_zipped = (dname_work / "support" / onlyfiles[0]).resolve()
-                fname_haplos = onlyfiles[0].split('.gz')[0]
-                fname_unzipped = (dname_work / "support" / fname_haplos).resolve()
-                # unzip
-                gunzip(fname_zipped, fname_result_haplos)
-
-            elif file.endswith('.fas'):
-                fname_haplos = (dname_work / "support" / onlyfiles[0]).resolve()
-                (dname_work  / "support"  / file).rename(fname_result_haplos)
-
-    # fix frequency information
-
-    freq_list = []
-    for record in SeqIO.parse(fname_result_haplos, "fasta"):
-        freq_list.append(float(record.description.split('ave_reads=')[-1]))
-    norm_freq_list = [float(i)/sum(freq_list) for i in freq_list]
-
-    record_list = []
-    for idx, record in enumerate(SeqIO.parse(fname_result_haplos, "fasta")):
-        record.description = f"freq:{norm_freq_list[idx]}"
-        record_list.append(record)
-    SeqIO.write(record_list, fname_result_haplos, "fasta")
+    open(fname_result_haplos, 'a').close()
 
 
 if __name__ == "__main__":
