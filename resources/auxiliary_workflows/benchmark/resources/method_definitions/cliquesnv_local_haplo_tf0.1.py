40bea267e3db52338f3244f6da06d42e267c9d8f
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-04-18 15:12:09 +0200

    method_definitions: cliquesnv

--- resources/auxiliary_workflows/benchmark/resources/method_definitions/cliquesnv_local_haplo_tf0.1.py
+++ resources/auxiliary_workflows/benchmark/resources/method_definitions/cliquesnv_local_haplo_tf0.1.py
@@ -1,5 +1,5 @@
 # GROUP: global
-# CONDA: cliquesnv = 2.0.3
+# CONDA: cliquesnv = 2.0.2
 # CONDA: samtools = 1.15.1
 # CONDA: biopython = 1.79
 
@@ -17,69 +17,77 @@ def main(
     fname_result_haplos,
     dname_work,
     seq_type,
-    threads=1,
+    threads,
 ):
     dname_work.mkdir(parents=True, exist_ok=True)
 
-    # prepare environment
-    subprocess.run(
-        ["samtools", "view", "-h", "-o", dname_work / "reads.sam", fname_bam],
-        check=True,
-    )
-
-    cliquesnv_mode = None
     if seq_type == "illumina":
-        cliquesnv_mode = "snv-illumina"
-    elif seq_type == "pacbio":
-        cliquesnv_mode = "snv-pacbio"
-    elif seq_type == "nanopore":
-        cliquesnv_mode = "snv-pacbio"
+        # create empty vcf files
+        open(fname_result_haplos, "a").close()
+        f = open(fname_results_snv, "a")
+        f.write("#CHROM     POS     ID      REF     ALT     QUAL    FILTER  INFO")
+        f.close()
+
     else:
-        raise RuntimeError(f"Invalid sequence technology: {seq_type}")
+        # prepare environment
+        subprocess.run(
+                ["samtools", "view", "-h", "-o", dname_work / "reads.sam", fname_bam],
+                check=True,
+        )
 
-    # execute tool
-    subprocess.run(
-        [
-            "cliquesnv",
-            "-m",
-            cliquesnv_mode,
-            "-in",
-            dname_work / "reads.sam",
-            "-outDir",
-            dname_work / "output",
-            "-tf",  # parameter to detect low-frequent mutations
-            "0.1",
-            "-tl",  # maximal runtime parameter
-            "2*428400",  # 119h*60*60
-            "-threads",
-            str(threads),
-            "-Xms612m",
-            "-Xmx40g",
-        ],
-        check=True,
-    )
-    fname_cliquesnv = dname_work / "output" / "reads.fasta"
+        runtime_limit = 15*24*60*60 - 60*60  # 15 days - 1h
+        cliquesnv_mode = None
+        if seq_type == "illumina":
+                cliquesnv_mode = "snv-illumina"
+        elif seq_type == "pacbio":
+                cliquesnv_mode = "snv-pacbio"
+        elif seq_type == "nanopore":
+                cliquesnv_mode = "snv-pacbio"
+        else:
+                raise RuntimeError(f"Invalid sequence technology: {seq_type}")
 
-    # fix frequency information
-    if fname_cliquesnv.exists():
-        record_list = []
-        for record in SeqIO.parse(fname_cliquesnv, "fasta"):
-            _, _, freq = record.id.split("_")
-            record.description = f"freq:{freq}"
+        # execute tool
+        subprocess.run(
+                [
+                "cliquesnv",
+                "-m",
+                cliquesnv_mode,
+                "-in",
+                dname_work / "reads.sam",
+                "-outDir",
+                dname_work / "output",
+                "-tf",  # parameter to detect low-frequent mutations
+                "0.1",
+                "-tl",  # maximal runtime parameter
+                str(runtime_limit),  # 119h*60*60
+                "-threads",
+                str(threads),
+                "-Xmx50G", # -Xmx50G
+                ],
+                check=True,
+        )
+        fname_cliquesnv = dname_work / "output" / "reads.fasta"
 
-            seq_nodel = str(record.seq).replace("-", "")
-            record.seq = Seq(seq_nodel)
+        # fix frequency information
+        if fname_cliquesnv.exists():
+                record_list = []
+                for record in SeqIO.parse(fname_cliquesnv, "fasta"):
+                        _, _, freq = record.id.split("_")
+                        record.description = f"freq:{freq}"
 
-            record_list.append(record)
-    else:
-        record_list = []
+                        seq_nodel = str(record.seq).replace("-", "")
+                        record.seq = Seq(seq_nodel)
 
-    SeqIO.write(record_list, fname_result_haplos, "fasta")
+                        record_list.append(record)
+    #else:
 
-    # create empty vcf files
-    f = open(fname_results_snv, "a")
-    f.write("#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO")
-    f.close()
+    #    record_list = []
+                SeqIO.write(record_list, fname_result_haplos, "fasta")
+
+        # create empty vcf files
+        f = open(fname_results_snv, "a")
+        f.write("#CHROM POS     ID      REF     ALT     QUAL    FILTER  INFO")
+        f.close()
 
 
 if __name__ == "__main__":
