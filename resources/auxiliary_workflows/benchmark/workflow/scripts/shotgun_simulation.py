5bd565d29f5957a3697e9f7e58f6122989f10357
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-04-18 10:43:30 +0200

    fix illumina simulation with indels in haplotypes, bwa is not aligning them correctly, hence use minimap2

--- resources/auxiliary_workflows/benchmark/workflow/scripts/shotgun_simulation.py
+++ resources/auxiliary_workflows/benchmark/workflow/scripts/shotgun_simulation.py
@@ -18,7 +18,7 @@ def simulate_illumina(fname_haplotype, coverage_haplotype, read_length, art_pref
     subprocess.run(
         [
             "art_illumina",
-            "-sam",
+            "-sam", # this alignment fails when we simulate indels in the haplotypes
             "--paired",
             "-m",
             str(
@@ -280,6 +280,30 @@ def main(
         )
         subprocess.run(["samtools", "sort", "-o", fname_bam, fd_tmp.name])
 
+    if (params["seq_tech"] == "illumina") & (haplotype_generation == "mutation_rate"):
+        deletion_rate = float(params["haplos"].split("@")[-2])
+        insertion_rate = float(params["haplos"].split("@")[-3])
+        # bwa is not able to align the simulated indels correctly.vim
+
+        if (insertion_rate>0) or (deletion_rate>0):
+                subprocess.run(["rm", fname_bam])
+                # align reads
+                subprocess.run(
+                    [
+                        "minimap2",
+                        "-ax",
+                        "sr",
+                        "--secondary=no",
+                        str(dname_work.resolve())[:-4] +"reference.fasta",
+                        str(fname_fastq.resolve()),
+                        "-o",
+                        str(art_prefix) + ".sam",
+                    ]
+                )
+
+                subprocess.run(["samtools", "sort", str(art_prefix) + ".sam", "-o", str(art_prefix) + ".sam"])
+
+                subprocess.run(["samtools", "view", "-bS", str(art_prefix) + ".sam", "-o", fname_bam])
 
 if __name__ == "__main__":
     main(
